0dca4a21c9c23f5d990e2af09f6692620cbc6ef8
Meghan Flynn <mflynn333@gmail.com>
2020-01-08 22:45:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster 
+    @roster
+  end 
+  
+  def add_student(name, grade)
+  end 
+  
+  
+end 
+
+
+
+
+
